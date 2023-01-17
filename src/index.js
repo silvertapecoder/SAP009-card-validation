8bd9e02d53d129f97c25210b3a3fc943b5b4a614
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-01-17 14:26:30 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+var capturando = "";
+
+function capturar() {
+    capturando = document.getElementById('valor').value;
+    document.getElementById('valorDigitado').innerHTML = capturando;
+}
