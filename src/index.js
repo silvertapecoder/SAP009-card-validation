bc434163d012377dfd5118d7aae2cee9f56e5aa4
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-01-20 15:54:30 -0300

    tem coisas funcionando

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ import validator from './validator.js';
 
 console.log(validator);
 
-var capturando = "";
+let cardNumber = document.getElementById("numero");
+let reversedCardNumber = cardNumber.reverse();
+console.log('este é o numero invertido' + reversedCardNumber + '.')
 
-function capturar() {
-    capturando = document.getElementById('valor').value;
-    document.getElementById('valorDigitado').innerHTML = capturando;
+function cardvalidation (event){
+    event.preventDefault()
+
+    const numeroDigitado = number.value
+    const validado = validator.isValid(numeroDigitado)
+
+    if(
+        numeroDigitado === "" ||
+        numeroDigitado === "0000000000000000" ||
+        numeroDigitado === "000000000000000" ||
+        numeroDigitado === "00000000000000" ||
+        numeroDigitado.length <= 13){
+            return (message.innerHTML = "Por favor, digite um número válido!")
+        }
+
+    else if(validado === true){
+        return(message.innerHTML = "Velidado")
+    }
+
+    else if(validado === false){
+        return(message.innerHTML = "Não validado")
+    }
+}
+
+number.addEventListner("keyup", clone);
+function clone (){
+    const value = document.getElementById("numero").value
+
+    const ocult= validator.maskify(value)
+    
+    const clone= document.getElementById("clone").value = ocult
 }
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,26 @@
+
+//inverter números
+
 const validator = {
-  // ...
+  reversedCardNumber
 };
+  
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+  //split numbers typed on valor
+  //separar números das posições pares (1,3,5,7,9,11,13,15)
+  //multiplicar por 2
+  //somar dígitos dos números a partir de 10 (if alguma coisa)
+  //somar todos os dígitos
 
 export default validator;
