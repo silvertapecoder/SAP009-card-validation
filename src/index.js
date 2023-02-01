dada93474150a0d9178af1f63b8d3575af43e6ce
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-02-01 08:56:27 -0300

    TERMINEIIII

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
-//import validator from './validator.js';
+/* eslint-disable no-console */
+import validator from './validator.js';
 
-//console.log(validator);
+console.log(validator);
 
-let creditCardNumber = document.getElementById("number")
-//let numberArray = cardNumber.split("");
-//let reversedCardNumber = numberArray.reverse();
-let button = document.getElementById("btn")
-let message = document.getElementById("mensagem")
+const creditCardNumber = document.getElementById("number");
+const button = document.getElementById("btn");
+const message = document.getElementById("mensagem");
 
-button.addEventListener('click', cardvalidation)
+function cardvalidation (e){
+  e.preventDefault();
+  const numberInput = creditCardNumber.value;
+  console.log(numberInput);
 
-
-
-function cardvalidation (event){
-    event.preventDefault()
-    const numberInput = creditCardNumber.value
-
-   // const validado = validator.isValid(numeroDigitado)
-
-    
+  const validador = validator.isValid(numberInput);
+  console.log(validador);
+      
    
-   if(numberInput.length <= 13){
-            return (message.innerHTML = "Número incompleto!")
-        }
+  if(numberInput.length < 10){
+    return (message.innerHTML = ` Número ${validator.maskify(numberInput)} incompleto! `)
+  }
 
-   
+  else if (validador) {
+            
+    return(message.innerHTML = ` Cartão ${validator.maskify(numberInput)} Válido `)
+  }
+  else {
+    return(message.innerHTML = ` Cartão ${validator.maskify(numberInput)} Inválido `)
+  } 
+
+
 
     
 }
 
-    
+button.addEventListener('click', cardvalidation); 
--- src/validator.js
+++ src/validator.js
@@ -1,75 +1,49 @@
 
-//inverter números
+
 
 const validator = {
  
   isValid(creditCardNumber){
-    /*const arrayCard =  [];
-    for (let i = 0; i < creditCardNumber.legth; i++){
-      const digit = parseInt(creditCardNumber[i]);
-      arrayCard.push(digit);
+    
+    const arrayCard = creditCardNumber.split('').map(Number).reverse();
+    let soma = 0;
+    
+    for (let i = 0; i < arrayCard.length; i++){
+      
+       
+      if (i % 2 !== 0){
+        const digit = arrayCard[i];
+        const multiply = digit * 2;
+  
+        if (multiply >= 10){
+          const sumDigits = multiply -9;
+    
+          soma += sumDigits;
+        }
+
+        else{
+          soma += multiply;
+        }
+      }
+
+      else{
+        soma += arrayCard[i];
+      } 
     }
 
-    const fromBackToFront = arrayCard.reverse();
+    return soma % 10 === 0    
+  },
 
-    for (let i = 0; i < fromBackToFront)
-  }*/
+  maskify(creditCardNumber) {
+    const fourNumbers = creditCardNumber.slice(-4);
+    const quantityOfNumbers = creditCardNumber.length -4;
+    const mask = "#".repeat(quantityOfNumbers) + fourNumbers;
 
-  let arrayCard = creditCardNumber.split('');
-  
-  let cardNumberReversed = arrayCard.reverse();
-
-  function multiplyEvenNumbers (){
-  
-    let evenNumbers = [];
-    
-    for (let i = 0; i < creditCardNumber.legth; i++){
-      const digit = creditCardNumber[i];
-      if (digit % 2 === 0){
-        evenNumbers.push(digit);
-      }
-
-      if else (i => 10){
-        const sumDigits = i -9;
-        evenNumbers.push(sumDigits);
-      }
-    }    
+    return mask
+   
   }
  
-  function SumOfDigits(){
-    let numbersSummedUp = 0;
-    for(let i= 0; i<evenNumbers.length; i++){
-      numbersSummedUp = numbersSummedUp + evenNumbers[i];
-    
-      let oddNumbers = []
-    
-    }
 
-  numbersSummedUp.push(evenNumbers);
-
-
-
-  }
 }
-};
-
-  
-
-
-
-
-
-
-
- 
- 
- 
-
-
-  //split numbers typed on valor
-  //separar números das posições pares (1,3,5,7,9,11,13,15)
-  //multiplicar por 2
-  //somar dígitos dos números a partir de 10 (if alguma coisa)
-  //somar todos os dígitos
 
 export default validator;
